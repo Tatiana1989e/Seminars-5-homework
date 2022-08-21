c798569450fb591a58adbfbd9c39caee0b73b9ca
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-21 19:31:23 +0300

    seminars5homework task34

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] numbers = new int[4];
+
+void FillArray(int[] array, int min, int max)
+{
+ for (int i = 0; i<array.Length; i++ )
+ {
+ array[i] = new Random().Next(min, max);
+  }
+}
+
+void WritingAnArray(int[] array)
+{
+ for (int i = 0; i<array.Length; i++ )
+ {
+ Console.Write(array[i] + " ");
+  }
+ Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+{
+ int quantity = 0;
+ for (int i = 0; i<array.Length; i++ )
+ {
+ if (array[i] % 2 == 1)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+FillArray(numbers, 100, 1000);
+WritingAnArray(numbers);
+Console.WriteLine();
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"The number of even numbers in the array: {quantity}");
+*/
+
